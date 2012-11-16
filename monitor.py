ea27aed553348c9c2dc1e70ba91aac27f5927ff6
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-11-16 12:57:42 -0200

    Address pylint warnings
    Change style and add documentation to address warnings issued by pylint.
    
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- monitor.py
+++ monitor.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+NAS monitor script
+
+Configure directories for RRD files and HTML files in the variables below.
+Volumes are in (block device, alias) format. If you plan to add more volumes
+or hard disks reserve space for them in the CONF_MAX_* variables.
+'''
+
 import sys
 
-from synomon.monitor import *
+from synomon.monitor import UptimeMonitor, StatMonitor, LoadMonitor, MemMonitor
+from synomon.monitor import VolMonitor, HDMonitor, IOMonitor, NetMonitor
 from synomon.graph import Graph
 
-conf_update_time = 300
-conf_rrd_dir = "/opt/var/lib/monitor"
-conf_dest_dir = "/volume1/web/stats"
+CONF_RRD_DIR   = "/opt/var/lib/monitor"
+CONF_DEST_DIR  = "/volume1/web/stats"
 
-volumes  = [
+CONF_VOLUMES   = [
     ('/dev/md0', 'Sys'),
     ('/dev/vg1/volume_1', 'Vol1'),
     ('/dev/vg1/volume_2', 'Vol2'),
@@ -20,16 +28,13 @@ volumes  = [
     ('/dev/vg2/volume_6', 'Vol6')
 ]
 
-hds      = [ 'sda', 'sdb' ]
-ifaces   = [ 'eth0' ]
+CONF_HDS      = [ 'sda', 'sdb' ]
+CONF_IFACES   = [ 'eth0' ]
 
-max_hds  = 2
-max_vols = 10
-max_lan  = 1
+CONF_MAX_HDS  = 2
+CONF_MAX_VOLS = 10
+CONF_MAX_LAN  = 1
 
-#
-#
-#
 
 if __name__ == '__main__':
 
@@ -39,27 +44,31 @@ if __name__ == '__main__':
 
     if sys.argv[1] == 'show':
         for i in [ UptimeMonitor(), StatMonitor(), LoadMonitor(), MemMonitor(),
-                   VolMonitor(volumes, max_vols), HDMonitor(hds, max_hds),
-                   IOMonitor(hds, max_hds), NetMonitor(ifaces, max_lan) ]:
+                   VolMonitor(CONF_VOLUMES, CONF_MAX_VOLS),
+                   HDMonitor(CONF_HDS, CONF_MAX_HDS),
+                   IOMonitor(CONF_HDS, CONF_MAX_HDS),
+                   NetMonitor(CONF_IFACES, CONF_MAX_LAN) ]:
             i.show()
 
     elif sys.argv[1] == 'update':
         for i in [ UptimeMonitor(), StatMonitor(), LoadMonitor(), MemMonitor(),
-                   VolMonitor(volumes, max_vols), HDMonitor(hds, max_hds),
-                   IOMonitor(hds, max_hds), NetMonitor(ifaces, max_lan) ]:
-            i.update(conf_rrd_dir)
+                   VolMonitor(CONF_VOLUMES, CONF_MAX_VOLS),
+                   HDMonitor(CONF_HDS, CONF_MAX_HDS),
+                   IOMonitor(CONF_HDS, CONF_MAX_HDS),
+                   NetMonitor(CONF_IFACES, CONF_MAX_LAN) ]:
+            i.update(CONF_RRD_DIR)
 
     elif sys.argv[1] == 'report':
         print 'Generating report...'
-        graph = Graph(conf_rrd_dir, height=150)
-        graph.network(conf_dest_dir + '/g0.png')
-        graph.cpu(conf_dest_dir + '/g1.png')
-        graph.load(conf_dest_dir + '/g2.png')
-        graph.memory(conf_dest_dir + '/g3.png')
-        graph.hdtemp(hds, conf_dest_dir + '/g4.png')
-        graph.hdio(hds, conf_dest_dir + '/g5.png')
-        graph.hdtime(hds, conf_dest_dir + '/g6.png')
-        graph.volume(volumes, conf_dest_dir + '/g7.png')
+        GRAPH = Graph(CONF_RRD_DIR, height=150)
+        GRAPH.network(CONF_DEST_DIR + '/g0.png')
+        GRAPH.cpu(CONF_DEST_DIR + '/g1.png')
+        GRAPH.load(CONF_DEST_DIR + '/g2.png')
+        GRAPH.memory(CONF_DEST_DIR + '/g3.png')
+        GRAPH.hdtemp(CONF_HDS, CONF_DEST_DIR + '/g4.png')
+        GRAPH.hdio(CONF_HDS, CONF_DEST_DIR + '/g5.png')
+        GRAPH.hdtime(CONF_HDS, CONF_DEST_DIR + '/g6.png')
+        GRAPH.volume(CONF_VOLUMES, CONF_DEST_DIR + '/g7.png')
         
     else:
         print "Invalid command %s" % (sys.argv[1])
